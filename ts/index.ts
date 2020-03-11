fc2a59283092ae79c0611fb4b991255614d6cb2c
Alexey Safonov <alexey@safonov.pro>
2020-03-11 12:49:28 +0300

    Шаблон основного файла

---
+++ ts/index.ts
@@ -0,0 +1,11 @@
+import {TGost, TLang, TMap} from './types'
+import maps from './maps'
+
+export function transilt(input: string, lang: TLang = 'ru', gost: TGost = '7.79-2000'): string {
+
+  return ''
+}
+
+export function retranslit(input: string, lang: TLang = 'ru', gost: TGost = '7.79-2000'): string {
+  return ''
+}
