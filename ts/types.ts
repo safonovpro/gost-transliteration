afc08704197a2218b7095ebea65bee0915c16c50
Alexey Safonov <alexey@safonov.pro>
2020-03-11 12:49:16 +0300

    Типы

---
+++ ts/types.ts
@@ -0,0 +1,14 @@
+export type TGost = '7.79-2000'
+
+export type TLang = 'ru' | 'be' | 'uk' | 'bg' | 'mk'
+
+export type TMap = {
+  [gost: string]: {
+    [symbol: string]: {
+      [lang: string]: {
+        symbols: Array<string>,
+        getSymbol?: (input: string) => string
+      }
+    }
+  }
+}
